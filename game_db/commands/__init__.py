fca91de8bb4e1d958fbf1090abe6f453e444bb0d
alexander-kan <akan.zce@gmail.com>
2025-12-27 13:11:52 +0100

    Format code with black

--- game_db/commands/__init__.py
+++ game_db/commands/__init__.py
@@ -5,12 +5,21 @@ from __future__ import annotations
 from .base import Command
 from .command_handler import CommandHandler
 from .file_commands import GetFileCommand, RemoveFileCommand, SyncSteamCommand
-from .game_commands import (CountGamesCommand, CountTimeCommand,
-                            GetGameCommand, SteamGameListCommand,
-                            SwitchGameListCommand)
-from .menu_commands import (ClearMenuCommand, FileMenuCommand,
-                            GameListsMenuCommand, MainMenuCommand,
-                            ShowAdminCommandsCommand, ShowCommandsCommand)
+from .game_commands import (
+    CountGamesCommand,
+    CountTimeCommand,
+    GetGameCommand,
+    SteamGameListCommand,
+    SwitchGameListCommand,
+)
+from .menu_commands import (
+    ClearMenuCommand,
+    FileMenuCommand,
+    GameListsMenuCommand,
+    MainMenuCommand,
+    ShowAdminCommandsCommand,
+    ShowCommandsCommand,
+)
 
 __all__ = [
     "Command",
